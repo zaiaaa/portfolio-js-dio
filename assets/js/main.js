37d5f338c5c8be1eca7e3c7d7b1a62f8c917e06d
zaiaaa <gustavo.0801214@gmail.com>
2023-07-17 17:06:17 -0300

    alterações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
+
+function updateProfileData(profileData){
+
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name    
+    
+    const name = document.getElementById('profile.name')
+    name.innerHTML = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerHTML = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerHTML = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerHTML = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerHTML = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
+
 (async function() {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileData(profileData)
 })()
